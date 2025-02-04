37b7366dec1e16a88ce39b800dd313d872e0d920
Alexsei Farafonov <toykacoffee5@email.com>
2025-02-04 15:37:02 +0300

    Версия 0.1.1

--- knight.py
+++ knight.py
@@ -1,5 +1,17 @@
-from Piece import piece
+from piece import Piece
+"""Импорт из файла Piece"""
 
 class Knight(Piece):
-    super().__init__
+    """Создание класса Конь"""
+
+    def __init__(self,place_at,color):
+        super().__init__()
+        self.color=color
+        """Цвет"""
+        self.place_at=place_at
+        """Место атаки"""
+
+    def attack_squares(self):
+        """Функция список квадратов которые может атаковать фигура"""
+        return super().attack_squares()
     
