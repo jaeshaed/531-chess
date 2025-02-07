24f62e6e5b36e88fb11f51b40ceb99eaf09c7b00
Alexsei Farafonov <toykacoffee5@email.com>
2025-02-07 10:23:09 +0300

    Версия 0.1.3 Добавлена функция move_knight в которой мы проверяем куда может ходить фигура

--- knight.py
+++ knight.py
@@ -8,22 +8,36 @@ class Knight(Piece):
         super().__init__(color,place_at)
 
 
-    def move_knight(self,x,y,):
+    def move_knight(self):
         "Все возможные ходы коня"
 
-        x=None
-        y=None
+        # offsets = [ (x+2,y+1),
+        #         (x+2,y-1),
+        #         (x-2,y+1),
+        #         (x-2,y-1),
+        #         (x+1,y+2),
+        #         (x+1,y-2),
+        #         (x-1,y+2),
+        #         (x-1,y-2)
+        #         ]
 
-        """Все возможные коды"""
-        return [ (x+2,y+1),
-                (x+2,y-1),
-                (x-2,y+1),
-                (x-2,y-1),
-                (x+1,y+2),
-                (x+1,y-2),
-                (x-1,y+2),
-                (x-1,y-2)
-                ]
+        moves = []
+        """Проверка возможности хода"""
+        if self.place_at.file < 6:
+            if self.place_at.rank < 7:
+                dest = self.game.board.squares[self.place_at + 2][self.place_at + 1],
+                [self.place_at + 2][self.place_at - 1],
+                [self.place_at - 2][self.place_at + 1],
+                [self.place_at -2][self.place_at - 1],
+                [self.place_at + 1][self.place_at + 2],
+                [self.place_at + 1][self.place_at - 2],
+                [self.place_at - 1][self.place_at + 2],
+                [self.place_at - 1][self.place_at - 2]
+                if dest.is_empty():
+                    moves.append(dest)
+                elif dest.piece.color != self.color:
+                    moves.append(dest)
+        return moves
         
     def attack_squares(self):
         """Функция список квадратов которые может атаковать фигура"""
