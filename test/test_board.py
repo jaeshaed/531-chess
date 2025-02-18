b499a63bbd8db123f66e0c93763f0b14991eee15
jaeshaed <36546964+jaeshaed@users.noreply.github.com>
2025-02-18 13:41:48 +0300

    Добавил проверок.

--- test/test_board.py
+++ test/test_board.py
@@ -8,13 +8,22 @@ from board import Board
 class TestBoard(unittest.TestCase):
     """Проверки шахматной доски."""
 
+    def setUp(self):
+        self.board = Board()
+
     def test_board_is_empty(self):
-        board = Board()
         for file in 'abcdefgh':
             for rank in '12345678':
                 at = file + rank
                 with self.subTest(square=at):
-                    self.assertIsNone(board.squares[at].piece)
+                    self.assertIsNone(self.board.squares[at].piece)
+                    self.assertTrue(self.board.squares[at].is_empty())
+
+    def test_board_has_64_squares(self):
+        self.assertEqual(len(self.board.squares), 64)
+
+    def test_squares_are_false_when_the_board_is_empty(self):
+        self.assertFalse(self.board.squares)
 
 
 if __name__ == '__main__':
