8dc8b7a60c6ba4207495ff80867d51b3e314b587
jaeshaed <36546964+jaeshaed@users.noreply.github.com>
2025-02-22 15:14:52 +0300

    Добавил проверок.

--- test/test_board.py
+++ test/test_board.py
@@ -22,15 +22,24 @@ class TestBoard(unittest.TestCase):
     def test_board_has_64_squares(self):
         self.assertEqual(len(self.board.squares), 64)
 
-    def test_getting_square_by_attr(self):
+    def test_getting_square_by_getattr(self):
         square = self.board.squares.e5
         self.assertEqual(square.file, 'e')
         self.assertEqual(square.rank, '5')
 
-    def test_getting_square_by_item(self):
+    def test_getting_square_by_getitem(self):
         square = self.board.squares['d4']
         self.assertEqual(square.file, 'd')
         self.assertEqual(square.rank, '4')
+
+    def test_getting_multiple_squares(self):
+        squares = self.board.squares['a3,b8,c7']
+        self.assertEqual(squares[0].file, 'a')
+        self.assertEqual(squares[0].rank, '3')
+        self.assertEqual(squares[1].file, 'b')
+        self.assertEqual(squares[1].rank, '8')
+        self.assertEqual(squares[2].file, 'c')
+        self.assertEqual(squares[2].rank, '7')
 
     def test_squares_are_false_when_the_board_is_empty(self):
         self.assertFalse(self.board.squares)
