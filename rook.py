b93068faba14f987ae90ad09c9ee7366d87e890c
MaikovaKhusnulgatina <husnulgatina58@gmail.com>
2025-03-07 10:47:57 +0300

    ладья

--- rook.py
+++ rook.py
@@ -1,8 +1,26 @@
 from piece import Piece
 
 class Rook(Piece):
-    def __init__(self,color, place_at):
+    def __init__(self,color, place_at=None):
         super().__init__(color, place_at)
+
+    def color(self):
+        return self.color
+    
+    def __init__(self,color, board):
+        super().__init__(color, board)
+        self.moved = False
+
+    def valid_moves(self) -> Square:
+        moves = []
+        current_square = self.place_at
+
+        square = current_square.up
+        while square is not None and square.is_on_board():
+            if square.is_empty():
+                moves.append
+
+
 
 
 
