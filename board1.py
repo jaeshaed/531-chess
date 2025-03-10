d67d3436428217f439af07e99cc3c6dde329126e
pravinfruit5 <nixasd231@gmail.com>
2025-03-10 18:25:27 +0300

    Версия 2. Начало работы над конструктором __init__

--- board1.py
+++ board1.py
@@ -5,3 +5,9 @@ from king import King
 from bishop import Bishop
 from Queen import Queen
 from rook  import Rook
+
+"""Класс игровой доски"""
+class Board(object):
+    """Конструктор в котором реализовано размещение фигур"""
+    def __init__(self):
+        self._squares = []
