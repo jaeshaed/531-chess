2c04decb59077fcfebb42fc83d0301c7baef8a7d
pravinfruit5 <nixasd231@gmail.com>
2025-03-10 18:28:09 +0300

    Версия 3. Конец работы над конструктором __init__.

--- board1.py
+++ board1.py
@@ -11,3 +11,28 @@ class Board(object):
     """Конструктор в котором реализовано размещение фигур"""
     def __init__(self):
         self._squares = []
+        for i in range(8):
+            row = []
+            for j in range(8):
+                square = Square(i, j)
+                if i == 0 or i == 7 or j == 0 or j == 7:
+                    piece = None
+                else:
+                    if i % 2 != j % 2:
+                        piece = Pawn('WHITE') if i < 6 else Pawn('BLACK')
+                    elif i > 1 and i < 6 and j % 2 == 0:
+                        piece = Rook('WHITE') if i < 4 else Rook('BLACK')
+                    elif i > 1 and i < 6 and j % 2 == 1:
+                        piece = Knight('WHITE') if i < 4 else Knight('BLACK')
+                    elif i > 1 and i < 6 and j == 1:
+                        piece = Bishop('WHITE') if i < 4 else Bishop('BLACK')
+                    elif i > 1 and i < 6 and j == 6:
+                        piece = Queen('WHITE') if i < 4 else Queen('BLACK')
+                    elif i == 1:
+                        piece = King('WHITE')
+                    elif i == 6:
+                        piece = King('BLACK')
+                    else:
+                        piece = None
+                row.append(square)
+            self._squares.append(row)
