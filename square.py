0bcda78e7d53c369a7d11c7c5002f4186b17f87a
kleay <rinaz3333222@gmail.com>
2025-03-11 10:46:39 +0300

    версия 2

--- square.py
+++ square.py
@@ -1,22 +1,64 @@
-class Square:
+from color import Color
 
-    def __init__ (self, all_squares, file, rank):
+class Square:
+    def __init__(self, all_squares, row, col, occupant=None):
         self._all_squares = all_squares
-        self.file = file
-        self.rank = rank
-        self.piece = None
+        self._row = row
+        self._col = col
+        self._occupant = occupant
+        self._color = Color.BLACK if (row + col) % 2 == 0 else Color.WHITE
+        self._highlighted = False
+        self._highlight_color = None
+
+    @property
+    def file(self):
+        return self._row + 1
+
+    @property
+    def rank(self):
+        return str(self._col + 1)
+
+    @property
+    def occupant(self):
+        return self._occupant
+
+    @occupant.setter
+    def occupant(self, piece):
+        self._occupant = piece
+
+    @property
+    def color(self):
+        return self._color
+
+    @property
+    def highlighted(self):
+        return self._highlighted
+
+    @property
+    def highlight_color(self):
+        return self._highlight_color
 
     def is_empty(self):
-        return self.piece is None
+        return self._occupant is None
 
-    def is_downmost(self):
-        return self.rank == 1
-    
-    def is_topmost(self):
-        return self.rank == 8
+    def place_piece(self, piece):
+        self._occupant = piece
 
-    def is_leftmost(self):
-        return self.file == "A"
+    def remove_piece(self):
+        self._occupant = None
 
-    def is_rightmost(self):
-        return self.file == "H"
+    def highlight(self, color='yellow'):
+        self._highlighted = True
+        self._highlight_color = color
+
+    def unhighlight(self):
+        self._highlighted = False
+        self._highlight_color = None
+
+    def __str__(self):
+        return str(self._occupant) if self._occupant else '.'
+
+    def __repr__(self):
+        return (f"Square(row={self._row}, col={self._col}, occupant={self._occupant}, "
+                f"color={self._color}, highlighted={self._highlighted}, "
+                f"highlight_color={self._highlight_color})")
